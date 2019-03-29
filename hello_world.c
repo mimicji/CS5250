02f6184c7fa31097cabc284add5cc3bf5b6db4a8
Kaihang JI <jikaihang@pku.edu.cn>
2019-03-29 17:28:36 +0800

    support param

--- hello_world.c
+++ hello_world.c
@@ -1,12 +1,17 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 
 MODULE_LICENSE("GPL");
 
+static char * who = "default";
+module_param(who, charp, 0000);
+MODULE_PARM_DESC(who, "String of param <WHO>");
+ 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, %s\n", who);
 	return 0;
 }
 
